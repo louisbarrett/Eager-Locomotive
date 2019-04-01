7a0e6d6189fc126e7b07621c5130cc6f3753385d
Louis Barrett <louis.barrett@segment.com>
2019-04-01 10:29:46 -0700

    Updated deployment to take user input

--- deploy.tf
+++ deploy.tf
@@ -1,8 +1,8 @@
 module "KEK_Stack" {
   source = "./KekStack"
-  es_domain_name = ""
-  cloudtrail_s3_bucket = ""
-  failure_s3_bucket = ""
+  es_domain_name = "${var.es_domain_name}"
+  cloudtrail_s3_bucket = "${var.cloudtrail_s3_bucket}"
+  failure_s3_bucket = "${var.failure_s3_bucket}"
 }
 
 output "kibana_endpoint" {
